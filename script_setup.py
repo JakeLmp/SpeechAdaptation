9508b860957a796370b666cc1abe5602de49e632
Jakob <jakob.limpens@gmail.com>
2023-10-11 16:10:52 +0200

    data epochs object pipeline created

--- script_setup.py
+++ script_setup.py
@@ -8,12 +8,25 @@ import pathlib
 #################################
 
 # Data-related
-DATA_FILE = r"C:\Users\Jakob\Documents\repositories\SpeechAdaptation\data\data\dbc_data.csv"
-# DATA_FILE = "path/to/data/file.csv"                    # Expected format provided elsewhere
-SAVE_DIRECTORY = "path/to/save/directory"              # Directory name, NOT a file 
+DATA_FILE = r"C:\Users\Jakob\Documents\repositories\SpeechAdaptation\data\practice_data\EE_EU_adaptation_ERP_1_Ambi_after_EE_34567_BC.vhdr"
+# DATA_FILE = "path/to/data/file.vhdr"                   # Expects BrainVision format header file (.vhdr extension)
+SAVE_DIRECTORY = r"C:\Users\Jakob\Documents\repositories\SpeechAdaptation\results"
+# SAVE_DIRECTORY = "path/to/save/directory"              # Directory name, NOT a file 
 
-BAD_ELECTRODES = ['Electrode1', 'Electrode2']          # List electrode names that should be ignored
-BAD_SUBJECTS = ['ExampleID1', 'ExampleID2']            # List subject IDs that should be ignored
+# Data import args
+# see https://mne.tools/stable/generated/mne.io.read_raw_brainvision.html#mne.io.read_raw_brainvision
+DATA_ARGS = {'eog' :    ('HEOGL', 'HEOGR', 'VEOGb'),
+             'misc' :   'auto',
+             'scale' :  1.0}
+
+TARGET_STIM_ONSET_MARKER_NAME = 'Time 0'               # Name or value of marker indicating target stimulus onset
+
+REFERENCE_ELECTRODES = ['M1', 'M2']                    # List of reference electrode names
+BAD_ELECTRODES = []                                    # List electrode names that should be ignored
+# BAD_SUBJECTS = []                                      # List subject IDs that should be ignored
+
+T_MIN = -0.2                                           # Starting time of trials in seconds (if baseline segment should be included, starting time can be negative)
+T_MAX = 1.0                                            # Ending time of trials in seconds
 
 # Model selection
 # See https://scikit-learn.org/stable/modules/classes.html#module-sklearn.linear_model
@@ -42,16 +55,63 @@ N_JOBS = -1                                 # if -1, equal to number of CPU core
 # ----- DATA IMPORTS ----- #
 ############################
 
-import pandas as pd
 import mne
 mne.set_log_level('ERROR')
 
 DATA_PATH = pathlib.Path(DATA_FILE)
 
-df = pd.read_csv(DATA_PATH)
+data_raw = mne.io.read_raw_brainvision(DATA_PATH, 
+                                       **DATA_ARGS)
 
-subject_ids = df['Subject'].unique()
+data_raw.load_data()
+data_raw.set_eeg_reference(REFERENCE_ELECTRODES)
+data_raw.info['bads'] = BAD_ELECTRODES
 
+GOOD_CHANNELS = list(set(data_raw.ch_names) - set(REFERENCE_ELECTRODES) - set(BAD_ELECTRODES))
+
+_allowed_channel_types = ['ecg', 'eeg', 'emg', 'eog', 'exci', 
+                          'ias', 'misc', 'resp', 'seeg', 'dbs', 
+                          'stim', 'syst', 'ecog', 'hbo', 'hbr', 
+                          'fnirs_cw_amplitude', 'fnirs_fd_ac_amplitude', 
+                          'fnirs_fd_phase', 'fnirs_od', 'eyetrack_pos', 
+                          'eyetrack_pupil', 'temperature', 'gsr']
+
+try:
+    if len(data_raw.annotations) > 0:
+        events = mne.events_from_annotations(data_raw)
+    else:
+        events = mne.find_events(data_raw)
+except:
+    raise Exception('Unable to segment data (could not find event annotations)')
+
+from pprint import pprint
+print("Found event IDs:")
+pprint(events[1])
+
+marker_present = False
+if isinstance(TARGET_STIM_ONSET_MARKER_NAME, str):
+    for key in events[1].keys():
+        if TARGET_STIM_ONSET_MARKER_NAME in key:
+            marker_present = True
+elif isinstance(TARGET_STIM_ONSET_MARKER_NAME, int):
+    for val in events[1].values():
+        if TARGET_STIM_ONSET_MARKER_NAME == val:
+            marker_present = True
+else:
+    raise ValueError(f"Unrecognised marker value ({TARGET_STIM_ONSET_MARKER_NAME})")
+
+try:
+    assert marker_present
+except:
+    raise ValueError(f"Could not find target stimulus marker in event IDs ({TARGET_STIM_ONSET_MARKER_NAME})")
+
+#%%
+data_epochs = mne.Epochs(data_raw, events[0],
+                         event_id=[val for key, val in events[1].items() if 'Stimulus' in key],
+                         tmin = T_MIN,
+                         tmax = T_MAX)
+
+ #%%
 
 
 
